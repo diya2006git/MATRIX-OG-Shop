909312c882958e9d44c9cc78c19dd06ce0b8d3c5
diya2006git <ds0673745@gmail.com>
2026-03-02 11:52:10 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/MATRIX OG Shop/',
+  base: '/MATRIX-OG-Shop/',
   plugins: [react()],
 });
